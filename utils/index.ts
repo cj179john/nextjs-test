bf7322641e708a2d5dd03f20a0a32b3f39a2d2e6
cj179john <cj179john@gmail.com>
2022-07-30 15:48:46 +0000

    Deploying to gh-pages from @ cj179john/nextjs-test@575667a8ce3120a82bed9dbcc7a4a5baa38bb41c 🚀

--- utils/index.ts
+++
@@ -1,5 +0,0 @@
-export const convertArrayToString = (arr: string[]) =>
-    arr.reduce((result, day, idx) => {
-        const suffix = idx < arr.length - 1 ?  ', ' : '';
-        return  result + day + suffix;
-    }, '')
